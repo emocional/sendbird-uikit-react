6fd6b3ea45c02cf609593c63afc1106af5f87e7f
esteve <esteve@emocional.co>
2024-10-22 12:20:00 +0200

    fix: build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 echo "cambio dependencia de emo-front a local path de sendbird-uikit-react"
-sed -i '' 's/uikit-react.*/uikit-react": "file:\/\/..\/sendbird-uikit-react\/dist",/' ../emo-front/package.json
-echo "build del fork"
+sed -i 's/uikit-react.*/uikit-react": "file:\/\/..\/sendbird-uikit-react\/dist",/' ../emo-front/package.jsonecho "build del fork"
 yarn build
 echo "voy al path de emo-front"
 cd ../emo-front
 echo "install"
 pnpm i
-pnpm nx serve
