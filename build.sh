89662c5f639963ad381335ca1e6f1e3d568097f4
esteve <esteve@emocional.co>
2024-10-22 12:18:53 +0200

    style: sendbird chat styles v1

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 echo "cambio dependencia de emo-front a local path de sendbird-uikit-react"
-sed -i 's/uikit-react.*/uikit-react": "file:\/\/..\/sendbird-uikit-react\/dist",/' ../emo-front/package.json
+sed -i '' 's/uikit-react.*/uikit-react": "file:\/\/..\/sendbird-uikit-react\/dist",/' ../emo-front/package.json
 echo "build del fork"
 yarn build
 echo "voy al path de emo-front"
 cd ../emo-front
 echo "install"
 pnpm i
+pnpm nx serve
