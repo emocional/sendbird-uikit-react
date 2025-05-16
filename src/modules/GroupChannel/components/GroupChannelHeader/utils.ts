64da63031de0530b69e27f5e01c7a32a2c39a5d5
esteve <esteve@emocional.co>
2025-05-16 10:21:53 +0200

    feat: add company tag in channel header

--- src/modules/GroupChannel/components/GroupChannelHeader/utils.ts
+++ src/modules/GroupChannel/components/GroupChannelHeader/utils.ts
@@ -1,11 +1,7 @@
 import type { GroupChannel } from '@sendbird/chat/groupChannel';
 import { LabelStringSet } from '../../../../ui/Label';
 
-export const getChannelTitle = (
-  channel: GroupChannel,
-  currentUserId: string,
-  stringSet: { [label: string]: string },
-): string => {
+export const getChannelTitle = (channel: GroupChannel, currentUserId: string, stringSet: { [label: string]: string }): string => {
   const LABEL_STRING_SET: { [label: string]: string } = stringSet || LabelStringSet;
   if (!channel?.name && !channel?.members) {
     return LABEL_STRING_SET.NO_TITLE;
@@ -20,6 +16,12 @@ export const getChannelTitle = (
 
   return channel?.members
     .filter(({ userId }) => userId !== currentUserId)
-    .map(({ nickname }) => (nickname || LABEL_STRING_SET.NO_NAME))
+    .map(({ nickname }) => nickname || LABEL_STRING_SET.NO_NAME)
     .join(', ');
+};
+
+export const getUserCompany = (channel: GroupChannel, currentUserId: string): string | null => {
+  const currentUser = channel?.members?.find(({ userId }) => userId === currentUserId);
+  const company = currentUser?.metaData['company_name'];
+  return !!company ? (company as string) : null;
 };
